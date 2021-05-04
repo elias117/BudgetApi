fb93cfab551236a164ed53af15a02f0754eb09fa
Elias Zoghaib <elias.zoghaib@dal.ca>
2021-05-03 21:34:58 -0300

    I added several url endpoints

--- Routers/envelopes.js
+++ Routers/envelopes.js
@@ -20,12 +20,13 @@ router.param('id', (req, res, next, id) => {
     }
 });
 
-router.get('/', (req, res) => {
-    res.status(200).send(Envelopes);
-});
 
 router.get('/:id', (req, res) => {
     res.status(200).send(req.env);
+});
+
+router.get('/', (req, res) => {
+    res.status(200).send(Envelopes);
 });
 
 router.post('/', (req, res) => {
@@ -41,6 +42,42 @@ router.post('/', (req, res) => {
 router.delete('/:id', (req, res) => {
     Envelopes.splice(req.envIndex, 1);
     res.status(200).send(req.env);
+});
+
+router.put('/:id', (req, res) => {
+    const updatedEnv = {
+        id: Number(req.env.id),
+        name: req.body.name,
+        budget: Number(req.body.budget)
+    };
+    Envelopes[req.envIndex] = updatedEnv;
+    res.status(200).send(updatedEnv);
+});
+
+router.post('/transfer/:from/:to', (req, res) => {
+    const transferAmount = Number(req.body.amount);
+    const fromIndex = Envelopes.findIndex( e => {
+        return e.name.toLowerCase() == req.params.from.toLowerCase();
+    });
+    const toIndex = Envelopes.findIndex(e => {
+        return e.name.toLowerCase() == req.params.to.toLowerCase();
+    });
+
+    if(fromIndex === -1){
+        res.status(404).send(`${req.params.from} Envelope Doesnt Exist`);
+    }else if(toIndex === -1){
+        res.status(404).send(`${req.params.to} Envelope Doesnt exist`);
+    }
+
+    const fromEnv = Envelopes[fromIndex];
+
+    if (transferAmount <= fromEnv.budget){
+        Envelopes[fromIndex].budget -= transferAmount;
+        Envelopes[toIndex].budget += transferAmount;
+        res.status(200).send('Amount Transferred');
+    }else{
+        res.status(404).send(`Error: Not enough money to transfer from ${fromEnv.name}!`);
+    }
 });
 
 module.exports = router;
