7d833b9ed14e5ca56bdfe994cc26d7b0d8024f1c
Elias Zoghaib <elias.zoghaib@dal.ca>
2021-05-04 09:52:54 -0300

    I added morgan logging

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const envelopeRouter = require('./Routers/envelopes');
+const morgan = require('morgan');
+
 const app = express();
 const port = 4000;
+
+app.use((req, res, next)=> {
+  morgan('tiny');
+  next();
+});
+
 
 app.use('/envelopes', envelopeRouter);
 
