b93ce3368fc8dec3ad9085a3261dc3c7b0f319e9
Elias Zoghaib <elias.zoghaib@dal.ca>
2021-10-02 17:41:23 -0300

    Changed the value of port to PORT

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var cors = require("cors");
 
 const app = express();
 // eslint-disable-next-line no-undef
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 
 app.use((req, res, next) => {
     morgan("tiny");
